e76a17a2bc6f2e5014fdf9555a1bc1451e2c7789
OdimarMengarda <odimarmengarda84@gmail.com>
2025-04-22 19:50:16 -0300

    Create Teste.c

---
+++ .github/workflows/Teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h›
+  
+void testar() {
+// Função de teste simples
+assert(1 == 1); // verifica se 1 é igual a 1
+printf("Teste passou! \n");
+}
+int main() {
+testar();
+return 0;
+}
